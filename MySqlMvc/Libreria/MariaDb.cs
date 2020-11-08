ee4ff840814d61f84745d5de3f0b91091a527705
Leonardo Chuello <ljchuello@gmail.com>
2020-11-07 20:41:41 -0500

    .

--- MySqlMvc/Libreria/MariaDb.cs
+++ MySqlMvc/Libreria/MariaDb.cs
@@ -84,7 +84,7 @@ namespace MySqlMvc.Libreria
                     MySqlCommand mySqlCommand = new MySqlCommand();
                     mySqlCommand.Connection = mySqlConnection;
                     mySqlCommand.CommandType = CommandType.Text;
-                    mySqlCommand.CommandText = $"SHOW TABLES FROM {mariaDb.BaseDatos.ToLower()}";
+                    mySqlCommand.CommandText = $"SHOW TABLES FROM {mariaDb.BaseDatos}";
                     using (MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader())
                     {
                         while (mySqlDataReader.Read())
